bbe348888e7dfc63d46582da7334a4498734bba3
Niriel <elsnirielc@gmail.com>
2026-03-04 12:10:30 -0600

    Delete Bliblioteca_Athenaeum_Admin directory

--- Bliblioteca_Athenaeum_Admin/src/api/Session.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-public class Session {
-    public static String token = null;
-    public static int idUsuario = 0;
-    public static String nombres = "";
-    public static String rol = "";
-    public static String apellidos = "";
-}
