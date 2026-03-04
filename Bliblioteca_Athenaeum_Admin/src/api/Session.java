935d36cf5258353735a80b282117c1ed56b0d410
Niriel <elsnirielc@gmail.com>
2026-03-04 04:06:58 -0600

    Add files via upload
    JavaSwing

---
+++ Bliblioteca_Athenaeum_Admin/src/api/Session.java
@@ -0,0 +1,9 @@
+package api;
+
+public class Session {
+    public static String token = null;
+    public static int idUsuario = 0;
+    public static String nombres = "";
+    public static String rol = "";
+    public static String apellidos = "";
+}
